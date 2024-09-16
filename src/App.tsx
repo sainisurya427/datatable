44f4b540f093f06281187c34971a03b5904cc5fe
saurabh saini <suryasaini427@gmail.com>
2024-09-15 23:54:19 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,4 @@
-//  // src/App.tsx
-// import React, { useState, useEffect } from 'react';
-// import { DataTable } from 'primereact/datatable';
-// import { Column } from 'primereact/column';
-// import CustomSelectionPanel from './components/CustomSelectionPanel';
-// import axios from 'axios';
-// import { Artwork } from './types/Artworks';
-
-// const App: React.FC = () => {
-//   const [artworks, setArtworks] = useState<Artwork[]>([]);
-//   const [selectedArtworks, setSelectedArtworks] = useState<Artwork[]>([]);
-//   const [totalRecords, setTotalRecords] = useState<number>(0);
-//   const [page, setPage] = useState<number>(0);
-//   const [rowsPerPage, setRowsPerPage] = useState<number>(5);
-
-//   useEffect(() => {
-//     const fetchArtworks = async () => {
-//       try {
-//         const response = await axios.get(`https://api.artic.edu/api/v1/artworks?page=${page + 1}`);
-//         const data = response.data.data;
-//         const total = response.data.pagination.total;
-//         setArtworks(data);
-//         setTotalRecords(total);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//       }
-//     };
-
-//     fetchArtworks();
-//   }, [page, rowsPerPage]);
-
-//   const onRowSelect = (e: { value: Artwork[] }) => {
-//     setSelectedArtworks(e.value);
-//   };
-
-//   return (
-//     <div className="card">
-//       <DataTable
-//         value={artworks}
-//         selection={selectedArtworks}
-//         onSelectionChange={(e) => onRowSelect(e as { value: Artwork[] })}
-//         paginator
-//         rows={rowsPerPage}
-//         totalRecords={totalRecords}
-//         onPage={(e) => setPage(e.page)}
-//         rowsPerPageOptions={[5, 10, 20]}
-//         selectionMode="checkbox"
-//       >
-//         <Column selectionMode="multiple" headerStyle={{ width: '3em' }}></Column>
-//         <Column field="title" header="Title"></Column>
-//         <Column field="place_of_origin" header="Place of Origin"></Column>
-//         <Column field="artist_display" header="Artist"></Column>
-//         <Column field="inscriptions" header="Inscriptions"></Column>
-//         <Column field="date_start" header="Start Date"></Column>
-//         <Column field="date_end" header="End Date"></Column>
-//       </DataTable>
-
-//       {/* Custom Selection Panel */}
-//       <CustomSelectionPanel selectedArtworks={selectedArtworks} />
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
-
-
-import React, { useState, useEffect } from 'react';
+ import React, { useState, useEffect } from 'react';
 import { DataTable, DataTablePageEvent } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Checkbox } from 'primereact/checkbox';
@@ -91,8 +23,9 @@ const App: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [totalRecords, setTotalRecords] = useState<number>(0);
   const [selectedArtworks, setSelectedArtworks] = useState<Artwork[]>([]);
-  const [rowsPerPage] = useState<number>(10); // Removed setRowsPerPage as it's not being used
-  const [page, setPage] = useState<number>(0); // To track current page
+  const [rowsPerPage] = useState<number>(10);
+  const [page, setPage] = useState<number>(0);
+  const [selectAll, setSelectAll] = useState<boolean>(false); // State for external checkbox
 
   // Fetch artworks data from API
   const fetchArtworks = async (page: number) => {
@@ -120,6 +53,22 @@ const App: React.FC = () => {
     setSelectedArtworks(e.value);
   };
 
+  // Select all rows using the external Checkbox
+  const handleSelectAll = (e: { checked: boolean }) => {
+    setSelectAll(e.checked);
+    if (e.checked) {
+      setSelectedArtworks(artworks); // Select all visible artworks
+    } else {
+      setSelectedArtworks([]); // Deselect all
+    }
+  };
+
+  // Clear selected rows when button is clicked
+  const clearSelection = () => {
+    setSelectedArtworks([]);
+    setSelectAll(false);
+  };
+
   // Column template functions
   const titleBodyTemplate = (rowData: Artwork) => rowData.title;
   const placeOfOriginBodyTemplate = (rowData: Artwork) => rowData.place_of_origin;
@@ -131,6 +80,25 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Artworks Table</h1>
+      <div className="toolbar">
+        <Checkbox 
+          inputId="selectAll" 
+          onChange={handleSelectAll} 
+          checked={selectAll} 
+          label="Select All"
+        />
+        <label htmlFor="selectAll" className="p-checkbox-label">
+          Select All Rows
+        </label>
+        <Button 
+          label="Clear Selection" 
+          icon="pi pi-times" 
+          className="p-button-danger" 
+          onClick={clearSelection} 
+          style={{ marginLeft: '10px' }}
+        />
+      </div>
+
       <DataTable
         value={artworks}
         paginator
@@ -167,4 +135,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
