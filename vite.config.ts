ca2e7d91dd9cd80ec93141062eabca0dbc12aaac
saurabh saini <suryasaini427@gmail.com>
2024-09-15 23:10:38 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This ensures the server binds to the correct host for Render
+    port: 3000,      // Optional: Set a default port for local development
+  }
 })
-
-
