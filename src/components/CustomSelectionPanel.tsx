ba22b95213224638536225fd79998ea74c6d57ab
saurabh saini <suryasaini427@gmail.com>
2024-09-15 23:11:52 -0700

    Update CustomSelectionPanel.tsx

--- src/components/CustomSelectionPanel.tsx
+++ src/components/CustomSelectionPanel.tsx
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import { Product } from '../types';
-
-// interface CustomSelectionPanelProps {
-//   selectedUsers: Product[];
-// }
-
-// const CustomSelectionPanel: React.FC<CustomSelectionPanelProps> = ({ selectedUsers }) => {
-//   return (
-//     <div className="custom-panel">
-//       <h3>Selected Products</h3>
-//       <ul>
-//         {selectedUsers.map(product => (
-//           <li key={product.code}>{product.name} - {product.category}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default CustomSelectionPanel;
-
-
-
 // src/components/CustomSelectionPanel.tsx
 import React from 'react';
 import { Artwork } from '../types/Artworks';
